8476454b82c17763d1fc9b90db8c168c6b0d42af
hardbricked-69 <thesolver08@gmail.com>
2022-01-14 12:37:05 +0530

    Zeus_Tree

---
+++ lineage_r5x.mk
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Descendant stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from r5x device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
+PRODUCT_BRAND := Realme
+PRODUCT_DEVICE := r5x
+PRODUCT_MANUFACTURER := Realme
+PRODUCT_NAME := lineage_r5x
+PRODUCT_MODEL := Realme 5 Series
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+TARGET_VENDOR_PRODUCT_NAME := r5x
+TARGET_VENDOR_DEVICE_NAME := r5x
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="r5x" \
+    PRIVATE_BUILD_DESC="trinket-user 11 RQ3A.210805.001.A1 release-keys"
+
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+
+# Zeus
+TARGET_FACE_UNLOCK_SUPPORT := YES
+TARGET_SUPPORTS_BLUR := true
